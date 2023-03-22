3538c4bf9f64cda2c7216e9deff32eee6346d3ae
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-03-23 01:36:12 +0600

    map on countries done

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Country = (props) => {
+    const{name} = props.country;
+    return (
+        <div>
+            <h1>{name.common}</h1>
+        </div>
+    );
+};
+
+export default Country;
